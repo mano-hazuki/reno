de7d33bfb49a540419d23bf1b434cff4414d391b
mano-hazuki <77273686+mano-hazuki@users.noreply.github.com>
2024-02-25 18:40:15 +0900

    Fixed header z-index Improved component naming Changed LectureItem.tsx design to handle text overflow problem.

--- resources/js/Components/Header.tsx
+++ resources/js/Components/Header.tsx
@@ -1,20 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass, faUser } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
-	return (
-		<header className="sticky top-0 w-full h-fit px-2 flex-none flex flex-row justify-between items-center bg-white drop-shadow">
-			<img src={ "" } alt="Logo" className="p-4"></img>
+    return (
+        <header
+            className="sticky top-0 z-50 w-full h-fit px-2 flex-none flex flex-row justify-between items-center bg-white drop-shadow-sm">
+            <img src={ "" } alt="Logo" className="p-4"></img>
 
-			<div className="flex flex-row items-center">
-				<a href="/search" className="p-4">
-					<FontAwesomeIcon icon={faMagnifyingGlass} color={"gray"} />
-				</a>
-				<button type="button" className="p-4">
-					<FontAwesomeIcon icon={faUser} color={"gray"} />
-				</button>
-			</div>
-		</header>
-	);
+            <div className="flex flex-row items-center">
+                <a href="/search" className="p-4">
+                    <FontAwesomeIcon icon={ faMagnifyingGlass } color={ "gray" }/>
+                </a>
+                <button type="button" className="p-4">
+                    <FontAwesomeIcon icon={ faUser } color={ "gray" }/>
+                </button>
+            </div>
+        </header>
+    );
 }
--- resources/js/Components/Section.tsx
+++ resources/js/Components/Section.tsx
@@ -1,4 +1,4 @@
-import VideoItem from "@/Components/VideoItem";
+import LectureItem from "@/Components/LectureItem";
 import { Lecture } from "@/types";
 
 interface Props {
@@ -10,8 +10,8 @@ export default function Section({ name, lectures }: Props) {
 	return (
 		<section className="w-full h-fit p-4">
 			<h1 className="my-2 font-bold text-2xl">{ name }</h1>
-			<ul className="w-full h-fit my-4">
-				{ lectures.map(lecture => <VideoItem key={ lecture.id } lecture={ lecture }/>) }
+			<ul className="w-full h-fit my-4 flex flex-col justify-start items-center gap-2">
+				{ lectures.map(lecture => <LectureItem key={ lecture.id } lecture={ lecture }/>) }
 			</ul>
 		</section>
 	);
